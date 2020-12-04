45861ef0cf9962629ff9d6952eb1750cc37af406
jose luis <cortezluisjose@gmail.com>
2020-12-04 02:24:18 +0000

    Automatically backed up by Learn

--- app/controllers/postcontroller.rb
+++ app/controllers/postcontroller.rb
@@ -2,6 +2,9 @@ class PostController < ActionController::Base
   
   def new
 end
-  
+  def create
+  Post.create(title: params[:post][:title], description: params[:post][:description])
+  redirect_to posts_path
+end
   
 end
